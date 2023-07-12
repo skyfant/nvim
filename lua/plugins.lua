daf896484def7b142c82b7226d6a38dd81c8ae1f
skyfant <110726268+skyfant@users.noreply.github.com>
2023-07-12 20:54:41 +0200

    Update plugins.lua: remove packer.nvim bootstrap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,17 +1,3 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -39,9 +25,5 @@ return require('packer').startup(function(use)
     --LSP
     use 'neovim/nvim-lspconfig'
     use 'williamboman/mason.nvim'   -- LSP installer
-
-    if packer_bootstrap then
-        require('packer').sync()
-    end
         
 end)

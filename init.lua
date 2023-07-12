21d857d6896a47650bc18ce69b0597ca42ed8a2c
skyfant <110726268+skyfant@users.noreply.github.com>
2023-07-12 21:29:58 +0200

    Update init.lua: added telescope config

--- init.lua
+++ init.lua
@@ -17,6 +17,13 @@ require("mason").setup({
 })
 
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+
 -- keymaps
 
 -- clear seach buffer with <Esc> for stopping highlighting search results

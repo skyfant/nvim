9a5600cc4dea7f02d0e88bee4f931f38cab10ec0
skyfant <110726268+skyfant@users.noreply.github.com>
2023-07-13 12:37:26 +0200

    Update init.lua: added keybind for close nvim tree

--- init.lua
+++ init.lua
@@ -52,6 +52,7 @@ require("nvim-tree").setup({
 
 vim.keymap.set('n', '<leader>to', ':NvimTreeOpen<cr>')
 vim.keymap.set('n', '<leader>tf', ':NvimTreeFocus<cr>')
+vim.keymap.set('n', '<leader>tc', ':NvimTreeClose<cr')
 
 -- keymaps
 

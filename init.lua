2658a491a4844741511d5ef797e176bfe4b086b6
skyfant <110726268+skyfant@users.noreply.github.com>
2023-07-13 12:10:34 +0200

    Update init.lua: added keybind for nvim-tree

--- init.lua
+++ init.lua
@@ -24,6 +24,35 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 
+-- Nvim-Tree
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
+
+vim.keymap.set('n', '<leader>to', ':NvimTreeOpen<cr>')
+vim.keymap.set('n', '<leader>tf', ':NvimTreeFocus<cr>')
+
 -- keymaps
 
 -- clear seach buffer with <Esc> for stopping highlighting search results
